03bc99f4320b1e7cafc6a08b2f0eecf6a40b2c86
Agil Ghifari <m.agil.ghifari@gmail.com>
2023-12-07 13:25:11 +0700

    Menyelesaikan semua service yang mungkin dibutuhkan

--- src/main/java/com/example/semwebProject/Controller/MainController.java
+++ src/main/java/com/example/semwebProject/Controller/MainController.java
@@ -1,14 +1,19 @@
 package com.example.semwebProject.Controller;
 
+import com.example.semwebProject.Model.PhoneData;
 import com.example.semwebProject.Model.PhoneDataDTO;
-import com.example.semwebProject.Service.PhoneDataService;
+import com.example.semwebProject.Service.PhoneDataServiceImpl;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 public class MainController {
@@ -17,18 +22,31 @@ public class MainController {
     ModelMapper modelMapper;
 
     @Autowired
-    PhoneDataService phoneDataService;
+    PhoneDataServiceImpl phoneDataService;
 
     @GetMapping(value = "/hello")
     String hello_world() {
         return "Hello World";
     }
 
-//    @GetMapping(value="/query")
-//    String query(@RequestBody PhoneDataDTO phoneDataDTO) {
-//
-//        // Convert DTO to entity
-//
-//
-//    }
+    @GetMapping(value="/query")
+    ResponseEntity<HashMap<String, String>> query(@RequestBody PhoneDataDTO phoneDataDTO) {
+        PhoneData phoneData = modelMapper.map(phoneDataDTO, PhoneData.class);
+        try {
+            return new ResponseEntity<>(phoneDataService.query(phoneData), HttpStatus.OK);
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(value="/getNamesList/{pageNum}")
+    ResponseEntity<List<String>> getNamesList(@PathVariable int pageNum) {
+        try {
+            return new ResponseEntity<>(phoneDataService.getNamesList(pageNum), HttpStatus.OK);
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
---
+++ src/main/java/com/example/semwebProject/Service/PhoneDataServiceImpl.java
@@ -0,0 +1,190 @@
+package com.example.semwebProject.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import com.example.semwebProject.Model.PhoneData;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdfconnection.RDFConnection;
+
+import org.springframework.stereotype.Service;
+
+/*
+ * Note untuk Datatype Boolean asm:
+ * Krn dataset kita booleannya "No" ama "Yes" ngeceknya perlu kyk gini asm:
+ * -- FILTER (?3g = "No"^^xsdasm:boolean) [Kalau mau filter "No"]
+ * -- FILTER (?3g = "Yes"^^xsdasm:boolean) [Kalau mau filter "Yes"]
+ * Yang di pass ke function di bawah itu "Yes" dan "No" jadinya
+ */
+
+@Service
+public class PhoneDataServiceImpl implements PhoneDataService{
+
+    public static final String serviceURL = "http://localhost:3030/phone/";
+
+    public static final String[] keys = {
+            "name", "phone_name", "price", "brand_name", "front_cam", "back_cam", "bat_unit", "bat_amount", "3g",
+            "4g", "bluetooth", "gps", "sim", "wifi", "os", "ram_amount", "ram_unit", "proc", "strg_amount", "strg_unit" };
+
+    @Override
+    public HashMap<String, String> query(PhoneData phoneData) throws DatabaseConnectionFailedException {
+        String query = generateQuery(phoneData);
+        return callQuery(query);
+    }
+
+    @Override
+    public List<String> getNamesList(int pageNumber) throws DatabaseConnectionFailedException {
+        List<String> phoneNames = new LinkedList<>();
+
+        try (RDFConnection conn = RDFConnection.connect(serviceURL)) {
+            String query = "SELECT ?name WHERE {?sub <http://anti-semanticism.cat/name> ?name }" +
+                    "LIMIT 100 OFFSET " + (pageNumber * 100);
+            conn.querySelect(
+                    query,
+                    (qs) -> {
+                        phoneNames.add(qs.get("name").toString());
+                    }
+            );
+            return phoneNames;
+        }
+        catch (Exception e) {
+            throw new DatabaseConnectionFailedException("Failed to connect to database");
+        }
+    }
+
+    private static String generateQuery(PhoneData phoneData) {
+
+        String additionalCond = generateAdditionalCondition(phoneData);
+
+        String hasSpec = String.join("",
+                "asm:hasCamera[ asm:hasFrontCamera ?front_cam ; asm:hasRearCamera ?back_cam ] ;",
+                "asm:hasBattery [",
+                "asm:amount ?bat_amount ;",
+                "asm:unit ?bat_unit ] ;",
+                "asm:hasNetworking [",
+                "asm:has3G ?3g ;",
+                "asm:has4G ?4g ;",
+                "asm:hasBluetooth ?bluetooth ;",
+                "asm:hasGPS ?gps ;",
+                "asm:hasSimsCard ?sim ;",
+                "asm:hasWifi ?wifi ; ] ;",
+                "asm:hasOS ?os ;",
+                "asm:hasRAM [ asm:amount ?ram_amount ; asm:unit ?ram_unit ] ;",
+                "asm:processorAmount ?proc ;",
+                "asm:hasStorage [ asm:amount ?strg_amount ; asm:unit ?strg_unit ] ;");
+
+        String ret = String.join("",
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>",
+                "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>",
+                "PREFIX asm: <http://anti-semanticism.cat/>",
+                "SELECT DISTINCT * WHERE {",
+                "?s asm:hasSpecification [", hasSpec, "] ;",
+                "asm:name ?name ;",
+                "asm:phoneName ?phone_name ;",
+                "asm:price [ asm:amount ?price] ;",
+                "asm:brand ?brand .",
+                "?brand asm:name ?brand_name .",
+                additionalCond,
+                "} LIMIT 10");
+
+        return ret;
+    }
+
+    private static HashMap<String, String> callQuery(String q) throws DatabaseConnectionFailedException {
+
+        HashMap<String, String> returnValue = new HashMap<>();
+
+        for (String key : keys) {
+            returnValue.put(key, null);
+        }
+
+        try (RDFConnection conn = RDFConnection.connect(serviceURL)) {
+            conn.querySelect(
+                    q,
+                    (qs) -> {
+                        for (String key : keys) {
+                            RDFNode rdfNode = qs.get(key);
+
+                            String value;
+                            if (rdfNode.isLiteral()) {
+                                value = rdfNode.asLiteral().getLexicalForm();
+                            }
+                            else {
+                                value = rdfNode.toString();
+                            }
+                            returnValue.put(key, value);
+                        }
+                    });
+        }
+        catch (Exception e) {
+            throw new DatabaseConnectionFailedException("Failed to connect to database");
+        }
+        return returnValue;
+    }
+
+    // FILTER (?3g = "No"^^xsdasm:boolean)
+    private static String generateAdditionalCondition(PhoneData phoneData) {
+
+        String
+                name = phoneData.getName(),
+                brand = phoneData.getBrand(),
+                minRAM = phoneData.getMinRAM(),
+                maxRAM = phoneData.getMaxRAM(),
+                os = phoneData.getOs(),
+                priceMin = phoneData.getPriceMin(),
+                priceMax = phoneData.getPriceMax(),
+                storageMin = phoneData.getStorageMin(),
+                storageMax = phoneData.getStorageMax(),
+                has4g = phoneData.getHas4g(),
+                hasWifi = phoneData.getHasWifi();
+
+
+        String ret = "";
+
+        if (name != null) {
+            ret += "FILTER CONTAINS(lcase(?name), \"" + name.toLowerCase() + "\") .";
+        }
+        if (brand != null) {
+            ret += "FILTER CONTAINS(lcase(?brand_name), \"" + brand.toLowerCase() + "\") .";
+        }
+        if (minRAM != null) {
+            ret += "FILTER (?ram_amount >= " + Integer.parseInt(minRAM) + ") .";
+        }
+        if (maxRAM != null) {
+            ret += "FILTER (?ram_amount <= " + Integer.parseInt(maxRAM) + ") .";
+        }
+        if (os != null) {
+            ret += "FILTER CONTAINS(lcase(?os), \"" + os + "\") .";
+        }
+        if (priceMin != null) {
+            ret += "FILTER (?price >= " + Integer.parseInt(priceMin) + ") .";
+        }
+        if (priceMax != null) {
+            ret += "FILTER (?price <= " + Integer.parseInt(priceMax) + ") .";
+        }
+        if (storageMin != null) {
+            ret += "FILTER (?strg_amount >= " + Integer.parseInt(storageMin) + ") .";
+        }
+        if (storageMax != null) {
+            ret += "FILTER (?strg_amount <= " + Integer.parseInt(storageMax) + ") .";
+        }
+        if (has4g != null) {
+            ret += "FILTER (?4g = \"" + has4g + "\") .";
+        }
+        if (hasWifi != null) {
+            ret += "FILTER (?wifi = \"" + hasWifi + "\") .";
+        }
+
+        return ret;
+    }
+
+}
+
+class DatabaseConnectionFailedException extends Exception {
+
+    public DatabaseConnectionFailedException(String errorMessage) {
+        super(errorMessage);
+    }
+}
